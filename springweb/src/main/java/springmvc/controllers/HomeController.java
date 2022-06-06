823bd8f2ecfe3abd7d56b98b9b6fd5d460918721
marcov55 <marcovascogit@gmail.com>
2022-06-06 17:47:47 +0000

    springmvc

--- springweb/src/main/java/springmvc/controllers/HomeController.java
+++ springweb/src/main/java/springmvc/controllers/HomeController.java
@@ -4,20 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
+import springmvc.models.Tabuada;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
     public String index() {
-       return "index.";
+       return "index";
     }
     
     @RequestMapping("/tabuada")
     public String tabuada(Model model) {
-       int[] resultado = new int[10];
-       for(int pos = 0;pos < 10;pos++) {
-           resultado[pos] = (pos + 1) * 2;
-        }
-        model.addAttribute("resultado", resultado);
-        return "tabuada";
+         //int[] resultado = new int[10];
+         //for(int pos = 0;pos < 10;pos++) {
+         //    resultado[pos] = (pos + 1) * 2;
+         // }
+         // model.addAttribute("resultado", resultado);
+         // return "tabuada";
+         Tabuada tab = new Tabuada( 2);
+         model.addAttribute("resultado", tab.getResultados());
+         return "tabuada";
     }
 }
